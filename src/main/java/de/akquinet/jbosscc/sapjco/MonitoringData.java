0c66badd2fbdd47657a30f8af9657c1864e7a9e5
Carsten Erker <carsten.erker@akquinet.de>
2012-06-19 17:41:52 +0200

    first working version with jsf2

---
+++ src/main/java/de/akquinet/jbosscc/sapjco/MonitoringData.java
@@ -0,0 +1,54 @@
+package de.akquinet.jbosscc.sapjco;
+
+import com.sap.conn.jco.JCo;
+import com.sap.conn.jco.JCoDestination;
+import com.sap.conn.jco.JCoDestinationManager;
+import com.sap.conn.jco.JCoException;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import java.io.Serializable;
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Map;
+
+@ManagedBean
+public class MonitoringData implements Serializable
+{
+    private static final long serialVersionUID = -2403138958014741653L;
+
+    private String destinationName;
+    private JCoDestination destination;
+
+    public String getDestinationName()
+    {
+        return destinationName;
+    }
+
+    public void setDestinationName( String destinationName )
+    {
+        this.destinationName = destinationName;
+    }
+
+    public JCoDestination getDestination() throws JCoException
+    {
+        return destination;
+    }
+
+    public void chooseDestination()
+    {
+        try
+        {
+            destination = JCoDestinationManager.getDestination( destinationName );
+        }
+        catch ( JCoException e )
+        {
+            List<String> destinationIDs = JCo.getDestinationIDs();
+
+            FacesContext.getCurrentInstance()
+                    .addMessage( null, new FacesMessage(
+                            "Destination " + destinationName + " not found. Try any of these: " + destinationIDs ) );
+        }
+    }
+}
